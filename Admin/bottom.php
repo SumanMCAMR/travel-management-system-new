cc8b5caac658f3db8bd8c7283e314f873d9fc2d4
suman kumar <89798103+SumanMCAMR@users.noreply.github.com>
2023-05-02 22:13:33 +0530

    uploded all files in admin

---
+++ Admin/bottom.php
@@ -0,0 +1,17 @@
+<div class="copyright text-right">
+	<p style="padding-right:100px">Developed By: &copy; Suman</p>
+</div>
+<script src="js/jquery.scrollTo.js"></script>
+<script src="js/jquery.nav.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+				console.log('start')
+			},
+			end: function() {
+				console.log('stop')
+			}
+		});
+	});
+</script>
